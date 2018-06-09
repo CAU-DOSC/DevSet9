5a38d4cd89295aa819b76b60f9be426af2d365a3
nErumin <nerumin90@gmail.com>
2018-06-09 13:14:15 +0900

    add a base class of all sets.

---
+++ src/main/java/set/IntSet.java
@@ -0,0 +1,47 @@
+package set;
+
+/**
+ * Represent an unique integer set.
+ */
+public abstract class IntSet {
+    private int maxElement;
+    private int maxValue;
+
+    protected IntSet(int maxElement, int maxValue) {
+        this.maxElement = maxElement;
+        this.maxValue = maxValue;
+    }
+
+    /**
+     * Add a new integer to the set only if it is not already contained.
+     * @param number a new integer to add.
+     */
+    public abstract void insert(int number);
+
+    /**
+     * Get the current number of elements.
+     * @return the current number of elements.
+     */
+    public abstract int size();
+
+    /**
+     * Store sorted elements in the out container.
+     */
+    public abstract void report(int[] outContainer);
+
+    /**
+     * Get the maximum number of elements.
+     * @return the maximum number of elements.
+     */
+    public int getMaxElement() {
+        return maxElement;
+    }
+
+    /**
+     * Get the maximum value of element.
+     * @return the maximum value of element.
+     */
+    public int getMaxValue() {
+        return maxValue;
+    }
+}
