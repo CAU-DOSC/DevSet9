fb908c50ae23544d36c8be7e1a586cb83db4c8a0
nErumin <nerumin90@gmail.com>
2018-06-12 12:03:04 +0900

    Add skeleton codes for IntSetList.

---
+++ src/main/java/set/IntSetList.java
@@ -0,0 +1,30 @@
+package set;
+
+/**
+ * Represent an unique integer set implemented through a linked-list.
+ */
+public class IntSetList extends IntSet {
+    /**
+     * Initialize a new 'IntSetList' instance.
+     * @param maxElement the maximum number of elements.
+     * @param maxValue the maximum value of element.
+     */
+    public IntSetList(int maxElement, int maxValue) {
+        super(maxElement, maxValue);
+    }
+
+    @Override
+    public void insert(int number) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void report(int[] outContainer) {
+
+    }
+}
