0bc3637b67725c4f9d29095c38e7faa789c2957b
nErumin <nerumin90@gmail.com>
2018-06-12 12:50:36 +0900

    Implement LinkedList structure for IntSetList.

--- src/main/java/set/IntSetList.java
+++ src/main/java/set/IntSetList.java
@@ -1,5 +1,8 @@
 package set;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Represent an unique integer set implemented through a linked-list.
  */
@@ -26,5 +29,64 @@ public class IntSetList extends IntSet {
     @Override
     public void report(int[] outContainer) {
 
+    }
+
+    private static class LinkedList {
+        private Node headNode;
+
+        private LinkedList() {
+            // set the head as a dummy node.
+            this.headNode = new Node(0);
+        }
+
+        private void insertBack(Node newNode) {
+            Node tailNode = headNode;
+            while (tailNode.nextNode != null) {
+                tailNode = tailNode.nextNode;
+            }
+
+            tailNode.nextNode = newNode;
+        }
+
+        private int find(int number) {
+            int count = 0;
+
+            for (Node node = headNode.nextNode; node != null; node = node.nextNode) {
+                if (node.value == number) {
+                    return count;
+                }
+                ++count;
+            }
+
+            return -1;
+        }
+
+        private int size() {
+            int count = 0;
+
+            for (Node node = headNode.nextNode; node != null; node = node.nextNode) {
+                ++count;
+            }
+
+            return count;
+        }
+
+        private List<Integer> getElements() {
+            List<Integer> elements = new ArrayList<>();
+            for (Node node = headNode.nextNode; node != null; node = node.nextNode) {
+                elements.add(node.value);
+            }
+
+            return elements;
+        }
+    }
+
+    private static class Node {
+        private int value;
+        private Node nextNode;
+
+        private Node(int value) {
+            this.value = value;
+        }
     }
 }
