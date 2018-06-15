1637a0f283595329aa41ea896561034b8398ee40
minn951120 <minn6534@gmail.com>
2018-06-16 08:52:09 +0900

    modify array simulator name in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,8 +8,8 @@ public class Main {
     public static void main(String[] args) {
         IntSetSimulator listSimulator = new IntSetListSimulator();
         listSimulator.simulateAll();
-        listSimulator = new IntSetArrSimulator();
-        listSimulator.simulateAll();
+        IntSetSimulator arrSimulator = new IntSetArrSimulator();
+        arrSimulator.simulateAll();
         IntSetSimulator bstSimulator = new IntSetBstSimulator();
         bstSimulator.simulateAll();
         IntSetSimulator bitVectorSimulator = new IntSetBitVectorSimulator();
