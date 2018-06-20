0368e099a4dc00c014f1478cfea330fc28c53411
nErumin <nerumin90@gmail.com>
2018-06-21 01:40:49 +0900

    Refactor all simulators.

--- src/main/java/tester/IntSetSimulator.java
+++ src/main/java/tester/IntSetSimulator.java
@@ -1,9 +1,63 @@
 package tester;
 
+import set.IntSet;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public abstract class IntSetSimulator {
+    /**
+     * The maximum value of elements in IntSet.
+     */
+    public static final int MAX_VALUE = 100000000;
+
+    /**
+     * The maximum number of elements in IntSet.
+     */
+    public static final int MAX_ELEMENT = MAX_VALUE / 100;
+
+    /**
+     * The simulation number of operation.
+     */
+    public static final int TRY_NUMBER = 35000;
+
+    protected static List<Integer> makeRandomElements() {
+        List<Integer> elements = new ArrayList<>();
+        Random elementGenerator = new Random();
+
+        for (int i = 0; i < TRY_NUMBER; ++i) {
+            elements.add(elementGenerator.nextInt(MAX_VALUE + 1));
+        }
+
+        return elements;
+    }
+
+    protected static IntSet makeRandomCollection(Iterable<Integer> elements,
+                                                 Class<? extends IntSet> classObject) {
+        IntSet collection = null;
+
+        try {
+            collection = (IntSet) classObject.getConstructors()[0].newInstance(MAX_ELEMENT, MAX_VALUE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (int element : elements) {
+            collection.insert(element);
+        }
+
+        return collection;
+    }
+
     private long startMemory = 0;
     private long startTime = 0;
 
+    protected IntSet cachedCollection = null;
+
+    /**
+     * Simulate all operations of IntSet.
+     */
     public void simulateAll() {
         simulateInitialize();
         simulateInsert();
@@ -11,10 +65,10 @@ public abstract class IntSetSimulator {
         simulateReport();
     }
 
-    public abstract void simulateInitialize();
-    public abstract void simulateSize();
-    public abstract void simulateInsert();
-    public abstract void simulateReport();
+    protected abstract void simulateInitialize();
+    protected abstract void simulateSize();
+    protected abstract void simulateInsert();
+    protected abstract void simulateReport();
 
     protected void startSimulate(String simulationName) {
         Runtime runtime = Runtime.getRuntime();
